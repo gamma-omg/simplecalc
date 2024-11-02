5de36d4dfe0d5162b5927c7007cc4aa05a7ce272
Evgeny Vlasov <e.vlasov@rj-games.com>
2024-11-02 20:29:56 +0200

    Main application

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::io::Write;
+
 fn main() {
-    let mut input = String::new();
-    let _ = std::io::stdin().read_line(&mut input).unwrap();
-    let res = simplecalc::eval(&input).unwrap();
-    println!("{res}");
+    loop {
+        print!("[>] ");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        let bytes_read = std::io::stdin().read_line(&mut input).unwrap();
+        if bytes_read <= 1 {
+            break;
+        }
+
+        match simplecalc::eval(&input) {
+            Ok(res) => println!("[=] {res}"),
+            Err(e) => println!("[E] {e}"),
+        }
+
+        println!();
+    }
 }
